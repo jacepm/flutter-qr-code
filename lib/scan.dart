f79418d2431e9ef218121e50eb7083e2083714b7
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-04 17:15:06 +0800

    feat: add Scan page

---
+++ lib/scan.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Scan extends StatefulWidget {
+  Scan({Key key}) : super(key: key);
+
+  @override
+  _ScanState createState() => _ScanState();
+}
+
+class _ScanState extends State<Scan> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
