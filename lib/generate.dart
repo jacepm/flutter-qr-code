c01284449aa1b167199e0c5d86196268302bc89d
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-04 18:32:45 +0800

    FEAT: Add content for generate widget

--- lib/generate.dart
+++ lib/generate.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 
 class Generate extends StatefulWidget {
   Generate({Key key, this.title}) : super(key: key);
@@ -9,18 +10,52 @@ class Generate extends StatefulWidget {
 }
 
 class _GenerateState extends State<Generate> {
+  String qrData = 'https://github.com/jacepm';
+  TextEditingController qrController = TextEditingController();
+
+  void _qrCodeGenerator() {
+    setState(() {
+      qrData =
+          qrController.text.isEmpty ? 'https://flutter.dev' : qrController.text;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
+      body: SingleChildScrollView(
         padding: EdgeInsets.all(30.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[],
+        child: Container(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              QrImage(data: qrData),
+              SizedBox(height: 20.0),
+              Text('Get your link/text to the QR CODE'),
+              TextField(
+                controller: qrController,
+                decoration: InputDecoration(
+                  hintText: 'Enter the link/text here..',
+                ),
+              ),
+              SizedBox(height: 20.0),
+              FlatButton(
+                padding: EdgeInsets.all(10.0),
+                child: Text('GENERATE'),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                  side: BorderSide(color: Colors.blue, width: 3.0),
+                ),
+                onPressed: () {
+                  _qrCodeGenerator();
+                },
+              ),
+            ],
+          ),
         ),
       ),
     );
