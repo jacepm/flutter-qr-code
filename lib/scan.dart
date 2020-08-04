637a1b6417198ab632162a822898257cbf345b0b
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-04 17:41:09 +0800

    feat: add text and button for scan widget

--- lib/scan.dart
+++ lib/scan.dart
@@ -9,6 +9,8 @@ class Scan extends StatefulWidget {
 }
 
 class _ScanState extends State<Scan> {
+  String result = 'Not yet scanned!';
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,9 +20,37 @@ class _ScanState extends State<Scan> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[],
+          children: <Widget>[
+            _textBuilder(
+              text: 'RESULT',
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
+            _textBuilder(text: result, fontSize: 16.0),
+            SizedBox(height: 20),
+            FlatButton(
+              padding: EdgeInsets.all(10.0),
+              child: Text('Scan'),
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+                side: BorderSide(color: Colors.blue, width: 3.0),
+              ),
+              onPressed: () {},
+            ),
+          ],
         ),
       ),
+    );
+  }
+
+  Widget _textBuilder({String text, double fontSize, FontWeight fontWeight}) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: fontSize,
+        fontWeight: fontWeight ?? FontWeight.normal,
+      ),
+      textAlign: TextAlign.center,
     );
   }
 }
