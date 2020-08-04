53f80d0ecfb33e89268265b6bfec987e3c498506
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-04 17:29:45 +0800

    feat: add scaffold widget for generate and scan

--- lib/generate.dart
+++ lib/generate.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Generate extends StatefulWidget {
-  Generate({Key key}) : super(key: key);
+  Generate({Key key, this.title}) : super(key: key);
+  final String title;
 
   @override
   _GenerateState createState() => _GenerateState();
@@ -10,6 +11,18 @@ class Generate extends StatefulWidget {
 class _GenerateState extends State<Generate> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[],
+        ),
+      ),
+    );
   }
 }
--- lib/scan.dart
+++ lib/scan.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Scan extends StatefulWidget {
-  Scan({Key key}) : super(key: key);
+  Scan({Key key, this.title}) : super(key: key);
+  final String title;
 
   @override
   _ScanState createState() => _ScanState();
@@ -10,6 +11,16 @@ class Scan extends StatefulWidget {
 class _ScanState extends State<Scan> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Container(
+        padding: EdgeInsets.all(30.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[],
+        ),
+      ),
+    );
   }
 }
