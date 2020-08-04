8f0fa4aa830116e91a9c9ffb06e1776f409c36d1
Jc Mandawe <jcpmandawe@gmail.com>
2020-08-04 17:15:14 +0800

    feat: add Generate page

---
+++ lib/generate.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Generate extends StatefulWidget {
+  Generate({Key key}) : super(key: key);
+
+  @override
+  _GenerateState createState() => _GenerateState();
+}
+
+class _GenerateState extends State<Generate> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
